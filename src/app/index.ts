3d493fa7fb5daaefb0247a90f8b6d3003a6395e5
epitacio <epitacio.araujo@fornecedores.alloha.com>
2025-11-09 13:04:18 -0300

    refactor: move WhatsApp webhook validation logic to a separate module

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,9 +1,10 @@
-import express, { Request, Response } from "express";
+import express from "express";
 
 import dotenv from "dotenv";
 import path from "node:path";
 
 import { WhatsAppMessageRecive } from "@app/WebHooks/MetaAPI/WhatsappMessageRecive";
+import { WhatsAppWebHookValidation } from "./WebHooks/MetaAPI/WhatsAppWebHookValidation";
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
@@ -13,20 +14,7 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/whatsapp", (req: Request, res: Response) => {
-  const mode = req.query["hub.mode"];
-  const token = req.query["hub.verify_token"];
-  const challenge = req.query["hub.challenge"];
-  const verifyToken = process.env["WHATSAPP_API_TOKEN_VERIFY_TOKEN"];
-
-  if (mode === "subscribe" && token === verifyToken) {
-    console.log("✅ WEBHOOK VERIFIED");
-    return res.status(200).send(challenge);
-  }
-
-  console.log("❌ WEBHOOK VERIFICATION FAILED");
-  return res.status(403).send("Forbidden");
-});
+app.get("/whatsapp", WhatsAppWebHookValidation);
 
 app.post("/whatsapp", WhatsAppMessageRecive);
 
